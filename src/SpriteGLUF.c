c2f3ce30af53cd4f0ab007b8ca96ce5020bb364d
Toxa <untoxa@mail.ru>
2026-02-16 01:17:44 +0300

    enable some SFX

--- src/SpriteGLUF.c
+++ src/SpriteGLUF.c
@@ -7,8 +7,12 @@
 
 #include "levels.h"
 
-DECLARE_SFX(fx_charge);
-DECLARE_SFX(fx_discharge);
+DECLARE_SFX(sfx1jump);
+DECLARE_SFX(sfx3fall);
+DECLARE_SFX(sfx4lift);
+DECLARE_SFX(sfx5paint);
+DECLARE_SFX(sfx6noname);
+DECLARE_SFX(sfx11exitsound);
 
 extern UINT8 restart;
 extern UINT8 battery_count;
@@ -45,6 +49,7 @@ void GLUFLogic(void * custom_data) BANKED {
 	player_x = start_x, player_y = start_y;
 	Sprite * sprite_door = NULL;
 	SetSpriteAnim(THIS, anim_enter, ANIMATION_SPEED_ENTER);
+	ExecuteSFX(BANK(sfx11exitsound), sfx11exitsound, SFX_MUTE_MASK(sfx11exitsound), SFX_PRIORITY_NORMAL);
 	for (UINT8 i = 0; i != 42; ++i) {
 		YIELD;
 	}
@@ -65,6 +70,7 @@ void GLUFLogic(void * custom_data) BANKED {
 							UpdateMetatile(player_x, player_y + 1, TILE_DISAPPEARED);
 						}
 						SetSpriteAnim(THIS, anim_jump_right, ANIMATION_SPEED_JUMP);
+						ExecuteSFX(BANK(sfx1jump), sfx1jump, SFX_MUTE_MASK(sfx1jump), SFX_PRIORITY_NORMAL);
 						for (UINT8 i = 0; i != (16 / MOVE_SPEED); ++i) {
 							THIS->x += MOVE_SPEED;
 							YIELD;
@@ -80,6 +86,7 @@ void GLUFLogic(void * custom_data) BANKED {
 							UpdateMetatile(player_x, player_y + 1, TILE_DISAPPEARED);
 						}
 						SetSpriteAnim(THIS, anim_jump_left, ANIMATION_SPEED_JUMP);
+						ExecuteSFX(BANK(sfx1jump), sfx1jump, SFX_MUTE_MASK(sfx1jump), SFX_PRIORITY_NORMAL);
 						for (UINT8 i = 0; i != (16 / MOVE_SPEED); ++i) {
 							THIS->x -= MOVE_SPEED;
 							YIELD;
@@ -94,6 +101,7 @@ void GLUFLogic(void * custom_data) BANKED {
 		tile_below = level_buffer[player_y + 1][player_x];
 		if (lifting == TILE_LIFT_UP) {
 			SetSpriteAnim(THIS, anim_fall, ANIMATION_SPEED_FALL);
+			ExecuteSFX(BANK(sfx4lift), sfx4lift, SFX_MUTE_MASK(sfx4lift), SFX_PRIORITY_NORMAL);
 			for (UINT8 i = 0; i != (16 / LIFT_SPEED); ++i) {
 				THIS->y -= LIFT_SPEED;
 				YIELD;
@@ -110,6 +118,7 @@ void GLUFLogic(void * custom_data) BANKED {
 			}
 		} else if (lifting == TILE_LIFT_DOWN) {
 			SetSpriteAnim(THIS, anim_fall, ANIMATION_SPEED_FALL);
+			ExecuteSFX(BANK(sfx4lift), sfx4lift, SFX_MUTE_MASK(sfx4lift), SFX_PRIORITY_NORMAL);
 			for (UINT8 i = 0; i != (16 / LIFT_SPEED); ++i) {
 				THIS->y += LIFT_SPEED;
 				YIELD;
@@ -125,6 +134,7 @@ void GLUFLogic(void * custom_data) BANKED {
 					falling = TRUE;
 					player_y++;
 					SetSpriteAnim(THIS, anim_fall, ANIMATION_SPEED_FALL);
+					ExecuteSFX(BANK(sfx3fall), sfx3fall, SFX_MUTE_MASK(sfx3fall), SFX_PRIORITY_NORMAL);
 					for (UINT8 i = 0; i != (16 / FALL_SPEED); ++i) {
 						THIS->y += FALL_SPEED;
 						YIELD;
@@ -146,7 +156,7 @@ void GLUFLogic(void * custom_data) BANKED {
 					level_buffer[start_y][start_x] = TILE_DOOR;
 					sprite_door = SpriteManagerAdd(SpriteDoor, (start_x << 4) + (TILE_BUFFER_OFFSET << 3), start_y << 4);
 				}
-				ExecuteSFX(BANK(fx_discharge), fx_discharge, SFX_MUTE_MASK(fx_discharge), SFX_PRIORITY_NORMAL);
+				ExecuteSFX(BANK(sfx5paint), sfx5paint, SFX_MUTE_MASK(sfx5paint), SFX_PRIORITY_HIGH);
 			}
 		} else if (tile_below == TILE_BATT_CHARGER) {
 			if (charge_cooldown) {
@@ -154,13 +164,14 @@ void GLUFLogic(void * custom_data) BANKED {
 			} else if (charge <= CHARGE_MAXIMUM) {
 				++charge;
 				charge_cooldown = CHARGE_COOLDOWN;
-				ExecuteSFX(BANK(fx_charge), fx_charge, SFX_MUTE_MASK(fx_charge), SFX_PRIORITY_NORMAL);
+				ExecuteSFX(BANK(sfx6noname), sfx6noname, SFX_MUTE_MASK(sfx6noname), SFX_PRIORITY_NORMAL);
 			}
 		}
 		// exit
 		if (level_buffer[player_y][player_x] == TILE_DOOR) {
 			if (sprite_door) SpriteManagerRemoveSprite(sprite_door);
 			SetSpriteAnim(THIS, anim_exit, ANIMATION_SPEED_ENTER);
+			ExecuteSFX(BANK(sfx11exitsound), sfx11exitsound, SFX_MUTE_MASK(sfx11exitsound), SFX_PRIORITY_NORMAL);
 			for (UINT8 i = 0; i != 42; ++i) {
 				YIELD;
 			}
